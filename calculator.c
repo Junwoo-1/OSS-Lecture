d602065b70d41374d5ed8dd254fdcc8860cadc81
Lee Junwoo <a23320928@gmail.com>
2020-10-30 16:18:07 +0900

    Add Multiple Function

--- calculator.c
+++ calculator.c
@@ -8,9 +8,13 @@ int sub(int a, int b) {
 	return a - b;
 }
 
+int mlt(int a, int b) {
+	return a * b;
+}
+
 int main() {
 	int s, m, n;
-	printf("덧셈 : 1, 뺄셈 : 2 ");
+	printf("덧셈 : 1, 뺄셈 : 2 , 곱셈 : 3");
 	scanf("%d", s);
 	if ( s == 1 ) {
 		printf("더할 숫자 입력 1 : ");
@@ -26,5 +30,13 @@ int main() {
 		scanf("%d", n);
 		sub(m,n);
 	}
+	else if ( s == 3) {
+		printf("곱할 숫자 입력 1 : ");
+		scanf("%d", m);
+		printf("곱할 숫자 입력 2 : ");
+		scanf("%d", n);
+		mlt(m,n);
+	}
+
 	return 0;
 }
